3338eb9deabf3e5acc108611f4d96e4dd04ad5be
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2022-07-26 18:32:16 +0100

    Finished exercise b

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -55,12 +55,33 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+jonathanTwitter = users["Jonathan"]["twitter"]
 # 2. Get Erik's hometown
+erikHometown = users["Erik"]["home_town"]
 # 3. Get the list of Erik's lottery numbers
+erikLottery = users["Erik"]["lottery_numbers"]
 # 4. Get the species of Avril's pet Monty
+avrilPetSpecie = users["Avril"]["pets"][0]["species"]
 # 5. Get the smallest of Erik's lottery numbers
+erikMinLottery = min(erikLottery)
 # 6. Return an list of Avril's lottery numbers that are even
+avrilLottery = users["Avril"]["lottery_numbers"]
+avrilEvenLottery = [even for even in avrilLottery if even %2 == 0]
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+erikLottery.append(7)
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"]["home_town"] = "Edinburgh"
 # 9. Add a pet dog to Erik called "fluffy"
+users["Erik"]["pets"].append({"name": "fluffy", "species": "dog"})
 # 10. Add another person to the users dictionary
+users["Tom"] = {
+    "twitter": "Dont have one =/",
+    "lottery_numbers": [69, 420, 666, 3.141592, 42, 1.61803398875],
+    "home_town": "Ilheus",
+    "pets": [
+      {
+        "name": "Maurice",
+        "species": "Imaginary"
+      }
+    ]
+  }
