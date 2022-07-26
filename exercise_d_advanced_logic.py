4626a6dcac135bbddbd893a3ce86a4d3af9c9816
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2022-07-26 19:21:57 +0100

    Finished exercise d

--- exercise_d_advanced_logic.py
+++ exercise_d_advanced_logic.py
@@ -3,19 +3,39 @@
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # 1. Print out a list of the even integers:
-
+evenNumbers = [even for even in numbers if even %2 == 0]
+print(evenNumbers)
 
 # 2. Print the difference between the largest and smallest value:
-
+largest = max(numbers)
+smallest = min(numbers)
+dif = largest - smallest
+print(dif)
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
 
-
+for index in range(len(numbers)):
+    if numbers[index] == 2:
+        if numbers[index-1] == 2 or numbers[index+1] == 2:
+            print("True")
+            break
+        
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
 #    
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
-
+ignore = False
+sum = 0
+for index in range(len(numbers)):
+    if numbers[index] == 6:
+        ignore = True
+    elif numbers[index] == 7:
+        ignore = False
+        continue
+    if not ignore:
+        sum += numbers[index]
+print(sum)
+     
 
 # 5. HARD! Print the sum of the numbers. 
 #    Except the number 13 is very unlucky, so it does not count.
@@ -23,6 +43,16 @@ numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 #    HINT - You will need to track the index throughout the loop.
 #
 #    So [5, 13, 2] would have sum of 5. 
+sum = 0
+dontAddIndex = None
+for index in range(len(numbers)):
+    if numbers[index] == 13:
+        dontAddIndex = index + 1
+        continue
+    if dontAddIndex != index:
+        sum += numbers[index]
+
+print(sum)
 
 
 
